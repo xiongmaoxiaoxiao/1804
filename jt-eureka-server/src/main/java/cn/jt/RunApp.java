79de561c5854d1bd4afc2ec76169cb2919fc9f9f
xiongmaoxiaoxiao <358885884@qq.com>
2018-09-12 14:13:45 +0800

    eurekaserverDemo

---
+++ jt-eureka-server/src/main/java/cn/jt/RunApp.java
@@ -0,0 +1,14 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RunApp {
+	public static void main(String[] args) {
+		SpringApplication.run(RunApp.class, args);
+	}
+}
